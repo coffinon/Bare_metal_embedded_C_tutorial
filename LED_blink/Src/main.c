fc63c27131d4ce994c4fcebff7b656a78d819082
Kacper Kupiszewski <k.kupiszewski4@gmail.com>
2020-10-18 17:43:13 +0200

    now each LED is blinking whenever i press button

--- LED_blink/Src/main.c
+++ LED_blink/Src/main.c
@@ -1,17 +1,29 @@
 #include "stm32f103xb.h"
 
+void delay(void);
+
 
 int main(void){
-	 RCC -> APB2ENR |= RCC_APB2ENR_IOPAEN;	// Set APB2ENR to provide GPIOA with clock
+	 RCC -> APB2ENR |= RCC_APB2ENR_IOPCEN;	// Set APB2ENR to provide GPIOC with clock
 
-	 GPIOA -> CRL |= GPIO_CRL_MODE5_1;		// Set GPIOA_5 as output mode 2MHz
-	 GPIOA -> CRL &= ~GPIO_CRL_CNF5_0;		// Set GPIOA_5 as Push-Pull output
+	 GPIOC -> CRL |= GPIO_CRL_MODE2_1;		// Set GPIOB_8 as output mode 2MHz
+	 GPIOC -> CRL &= ~GPIO_CRL_CNF2_0;		// Set GPIOB_8 as Push-Pull output
+	 GPIOC -> CRL |= GPIO_CRL_MODE3_1;		// Set GPIOB_9 as output mode 2MHz
+	 GPIOC -> CRL &= ~GPIO_CRL_CNF3_0;		// Set GPIOB_9 as Push-Pull output
 
 	 while(1){
-		 GPIOA -> ODR |= GPIO_ODR_ODR5;		// Set GPIOA_5 output register
-		 for(uint32_t delay = 300000; delay > 0; --delay);
-		 GPIOA -> ODR &= ~GPIO_ODR_ODR5;	// Clear GPIOA_5 output register
-		 for(uint32_t delay = 300000; delay > 0; --delay);
+		 if(GPIOC -> IDR & GPIO_IDR_IDR13){
+			 GPIOC -> BSRR = GPIO_BSRR_BS2 + GPIO_BSRR_BR3;
+			 delay();
+		 }
+		 else{
+			 GPIOC -> BSRR = GPIO_BSRR_BR2 + GPIO_BSRR_BS3;
+			 delay();
+		 }
 	 } /* while(1) */
-
 } /* main */
+
+
+void delay(void){
+	for(uint32_t delay = 100000; delay > 0; --delay);
+}
